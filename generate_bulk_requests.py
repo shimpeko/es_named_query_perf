cdc13b580b5dadb3295a2903ab98bee9ded994c5
shimpeko <shimpeko@users.noreply.github.com>
2024-05-13 11:49:32 +0100

    Update generate_bulk_requests.py

--- generate_bulk_requests.py
+++ generate_bulk_requests.py
@@ -7,4 +7,4 @@ N = 100000
 for _ in range(N):
     text = ''.join(random.choices(string.ascii_lowercase, k=10))
     print(json.dumps({"index": {}}))
-    print(json.dumps({"doc": {"test_text": text}}))
+    print(json.dumps({"test_text": text}))
