b897f9199c3bd405b1165efd6adcd4aef5407ac3
Shimpei Kodama <shimpei-kodama@cookpad.com>
2024-05-16 09:41:08 +0100

    Update query to match documents

--- generate_query.py
+++ generate_query.py
@@ -1,13 +1,21 @@
 import string
 import random
 import json
+import pathlib
 
 N = 4000
 
 matches = []
-for _ in range(N):
-    text = ''.join(random.choices(string.ascii_lowercase, k=10))
-    matches.append({"match": {"test_text": {"query": text, "_name": f"query = {text}"}}})
+
+i = 0
+for req_body in pathlib.Path('bulk_requests.json').read_text().splitlines():
+    req = json.loads(req_body)
+    if not req.get("test_text"):
+        continue
+    if i >= N:
+        break
+    matches.append({"match": {"test_text": {"query": req["test_text"], "_name": f"query = {req['test_text']}"}}})
+    i = i + 1
 
 query = {
   "query": {
